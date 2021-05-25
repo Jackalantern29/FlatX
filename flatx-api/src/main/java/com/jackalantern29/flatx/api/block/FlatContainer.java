0cac2f3c7ec80cb57322d407740402bb9c426462
Jackalantern29 <mikalmck29@gmail.com>
2021-05-24 20:30:03 -0400

    Extends FlatInventoryHolder

--- flatx-api/src/main/java/com/jackalantern29/flatx/api/block/FlatContainer.java
+++ flatx-api/src/main/java/com/jackalantern29/flatx/api/block/FlatContainer.java
@@ -2,8 +2,9 @@ package com.jackalantern29.flatx.api.block;
 
 import com.jackalantern29.flatx.api.FlatBlockState;
 import com.jackalantern29.flatx.api.inventory.FlatInventory;
+import com.jackalantern29.flatx.api.inventory.FlatInventoryHolder;
 
-public interface FlatContainer extends FlatBlockState {
+public interface FlatContainer extends FlatBlockState, FlatInventoryHolder {
     FlatInventory getInventory();
     FlatInventory getSnapshotInventory();
 }
